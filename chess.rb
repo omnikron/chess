eabddaa43f9142f9ed4b5bdbd0d1968104f8ccb0
Oli Barnett <oli.barnett@gmail.com>
2011-07-17 17:49:48 +0200

    first commit

---
+++ chess.rb
@@ -0,0 +1,35 @@
+#Chessboard generator
+
+
+
+pieces = %w(| r | k | b | q | k | b | k | r |)
+pawns = Array.new(8, "| p")
+pawns[8] ='|'
+
+
+top = Array.new(32, '_')
+top[0] = ' '
+
+separator = Array.new(8, '---+')
+separator[0] = '|'
+separator[10] = '---|'
+
+empty_space = Array.new(8, '|   ')
+empty_space[8] = '|'
+
+puts top.join
+puts pieces.join(' ')
+puts separator.join('')
+puts pawns.join(' ')
+  4.times do
+    puts separator.join('')
+    puts empty_space.join('')
+  end
+puts separator.join('')
+puts pieces.join(' ').upcase
+puts separator.join('')
+puts pawns.join(' ').upcase
+puts top.join('')
+
+
+# 8.times
